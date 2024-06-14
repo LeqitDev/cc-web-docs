e15883c5e66d686ee84ddd73019ebb8f439d5b0d
CubeCoder <markus.hamacher16@gmail.com>
2024-06-14 16:15:44 +0200

    old

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,5 +1,26 @@
 import type { Load } from "@sveltejs/kit";
 import { compile } from "mdsvex";
+import { compile as svelteCompile } from "svelte/compiler";
+import mdx from "remark-mdx";
+
+export const prerender = true;
+export const ssr = false;
+
+function fromBinary(encoded: string) {
+    return decodeURIComponent(atob(encoded).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+  }
+
+  function blobToFile(theBlob: Blob, fileName:string): File {
+    const b: any = theBlob;
+    //A Blob() is almost a File() - it's just missing the two properties below which we will add
+    b.lastModifiedDate = new Date();
+    b.name = fileName;
+      
+    //Cast to a File() type
+    return theBlob as File;
+  }
 
 export const load: Load = async ({ params }) => {
         let response = await fetch('https://api.github.com/repos/LeqitDev/cc-docs/contents/' + params.slug + '?ref=main', {
@@ -9,6 +30,19 @@ export const load: Load = async ({ params }) => {
                 "Authorization": "Bearer " + import.meta.env.VITE_GITHUB_TOKEN
             }
         });
-        let data = atob(await response.json());
-        let mdsvex_obj = await compile(data);
+
+        const response_json = await response.json();
+        const data = fromBinary(response_json.content);
+        
+        const mdsvex_obj = await compile(data, {
+            extensions: [".svelte", ".svx", ".md", ".mdx"],
+            layout: "./src/lib/layouts/docs.svelte",
+        });
+        // console.log(mdsvex_obj?.code);
+
+        if (mdsvex_obj) {
+            return { obj: mdsvex_obj }
+        }
+        
+        return { obj: null }
 };
---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import type { PageData } from "./$types";
+	import { compile } from "svelte/compiler";
+
+    export let data: PageData;
+</script>
+{#if data.obj}
+    <div>{@html data.obj.code}</div>
+{/if}
